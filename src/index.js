c3d1c0a0ac6c9e6ad34d5e2b28d7157c2c728c8c
OlessyaOG <152127554+OlessyaOG@users.noreply.github.com>
2024-12-30 18:24:11 -0800

    html, js, css
    - html - added new classes
    - js - hidden box, added blinking animation (jas and css)
    - improved UX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 20,
+    delay: 50,
     cursor: "🖋️",
   });
 }
@@ -19,6 +19,10 @@ function generatePoem(event) {
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink"> ⌛ Generating a poem about ${instructionsInput.value}...</div>`;
 
   /// Make a call to API
 
